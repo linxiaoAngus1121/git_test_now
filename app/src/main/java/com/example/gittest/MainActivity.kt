20ae872334cbf2d9ca316f6ecc30df94324186bf
v-linxiao <linxiao001@chinasofti.com>
2021-11-23 15:13:33 +0800

    新增了MainActivity中的toast

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"你好啊，这是最原始的",Toast.LENGTH_LONG).show()
     }
 }
