36ee5b189414baf0a289954cec46132eae256cf2
v-linxiao <linxiao001@chinasofti.com>
2021-11-23 15:20:54 +0800

    点击了revert commit

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -8,7 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"你好啊，这是最原始的",Toast.LENGTH_LONG).show()
-        //我新增了一句注释，但是不提交到github
     }
 }
